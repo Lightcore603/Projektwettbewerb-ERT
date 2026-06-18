64f5c6c981505935644d607632bd86d254a3a2f8
Lightcore603 <st184671@stud.uni-stuttgart.de>
2026-06-18 15:24:55 +0200

    4.4 fertig, nix läuft

---
+++ A4_4.m
@@ -0,0 +1,120 @@
+%% Kapitel 4.4 - Zustandsrueckfuehrung
+
+clear; clc; close all;
+
+ml = 1;
+
+A = [ 0,      1, 0,      0;
+    -9.81*(1 + ml/0.8), 0, 0, 0.009;
+    0,      0, 0,      1;
+    12.26*ml, 0, 0, -0.009 ];
+
+B = [0; -30.49; 0; 30.49];
+
+Cy = [1 0 0 0;
+    0 0 1 0];
+
+Cx = [0 0 1 0];
+
+% Analyse
+disp('Eigenwerte der offenen Strecke:')
+disp(eig(A))
+
+disp('Rang Steuerbarkeit:')
+disp(rank(ctrb(A,B)))
+
+disp('Rang Beobachtbarkeit mit y=[phi;x]:')
+disp(rank(obsv(A,Cy)))
+
+% Zustandsrueckfuehrung
+pK = [-1.8 -2.1 -2.4 -2.7];
+K = place(A, B, pK);
+
+disp('K = ')
+disp(K)
+
+disp('Eigenwerte A-BK:')
+disp(eig(A-B*K))
+
+% Vorfilter fuer x-Ausgang
+V = -1 / (Cx * ((A - B*K) \ B));
+
+disp('V = ')
+disp(V)
+
+% Geschlossener Kreis von x_soll nach z
+Acl = A - B*K;
+Bcl = B*V;
+
+sys_x = ss(Acl, Bcl, Cx, 0);
+
+figure;
+step(2.45*sys_x, 20);
+grid on;
+title('Lineares Modell: x auf 2.45 m');
+
+% Stellmoment fuer Sprung testen
+t = 0:0.001:20;
+r = 2.45 * ones(size(t));
+[y,t,z] = lsim(ss(Acl,Bcl,eye(4),zeros(4,1)), r, t);
+
+M = zeros(size(t));
+for i = 1:length(t)
+    M(i) = -K*z(i,:).' + V*r(i);
+end
+
+figure;
+plot(t,M);
+grid on;
+xlabel('t [s]');
+ylabel('M [Nm]');
+title('Stellmoment M');
+yline(0.4,'--');
+yline(-0.4,'--');
+
+figure;
+subplot(2,1,1);
+plot(t,z(:,3)); grid on;
+ylabel('x [m]');
+title('Zustandsantwort');
+
+subplot(2,1,2);
+plot(t,z(:,1)); grid on;
+ylabel('\phi [rad]');
+xlabel('t [s]');
+
+
+%% Beobachterentwurf
+
+pObs = [-7 -8 -9 -10];
+
+H = place(A', Cy', pObs)';
+
+Fobs = A - H*Cy;
+E1 = H(:,1);
+E2 = H(:,2);
+Lobs = B;
+
+disp('H = ')
+disp(H)
+
+disp('Fobs = ')
+disp(Fobs)
+
+disp('E1 = ')
+disp(E1)
+
+disp('E2 = ')
+disp(E2)
+
+disp('Lobs = ')
+disp(Lobs)
+
+disp('Eigenwerte Beobachterfehlerdynamik:')
+disp(eig(A - H*Cy))
+
+% Fuer Simulink State-Space Beobachter:
+Aobs = Fobs;
+Bobs = [E1 E2 Lobs];
+Cobs = eye(4);
+Dobs = zeros(4,3);
--- templates/reglerInit.m
+++ templates/reglerInit.m
@@ -1,2 +1,109 @@
-%% Initialisierung des Reglers
- 
+%% reglerInit.m
+% Kapitel 4.4 - Zustandsrueckfuehrung mit Beobachter und Sollwertfilter
+
+
+
+%% Nominelles lineares Modell fuer ml = 1 kg
+
+ml_nom = 1;
+
+A = [ 0, 1, 0, 0;
+     -9.81*(1 + ml_nom/0.8), 0, 0, 0.009;
+      0, 0, 0, 1;
+      12.26*ml_nom, 0, 0, -0.009 ];
+
+B = [0; -30.49; 0; 30.49];
+
+C_t = [1 0 0 0;
+       0 0 1 0];
+
+Cx = [0 0 1 0];
+
+%% Stellgroessen- und Motorparameter
+
+Mmax_total = 0.4;
+Pmax = 3.56;
+uMax = 100;
+
+%% Zustandsrueckfuehrung
+
+% Vorsichtiger Startwert
+% Wenn das zu langsam ist: [-1.6 -1.8 -2.0 -2.2]
+% Wenn Constraints verletzt werden: [-1.2 -1.4 -1.6 -1.8]
+pK = [-20 -20.1 -20.2 -20.3];
+
+K = place(A, B, pK);
+
+% Statischer Vorfilter fuer Form M = -K*xhat + V*x_soll
+% Bei Fehlervektor [phi_hat; phidot_hat; x_hat-x_ref; xdot_hat]
+% ist V effektiv gleich K(3) und muss nicht separat verwendet werden.
+V = -1 / (Cx * ((A - B*K) \ B));
+
+%% Beobachter
+
+% Beobachter nicht zu schnell machen, sonst wird der Regler hektisch.
+%pObs = [-30 -31 -32 -33];
+pObs= 3*pK
+
+L_t = place(A', C_t', pObs)';
+
+%% Variablen fuer manuellen Beobachter
+
+A_t = A;
+B_t = B;
+xhat0 = [0; 0; 0; 0];
+
+%% Optional: Beobachter als State-Space-Block
+
+Aobs = A - L_t*C_t;
+Bobs = [L_t(:,1), L_t(:,2), B];
+Cobs = eye(4);
+Dobs = zeros(4,3);
+
+%% Dynamischer Sollwertfilter / Vorfilter
+
+% Konservativer Startwert:
+wRef = 0.45;
+zetaRef = 1.0;
+
+numRef = wRef^2;
+denRef = [1, 2*zetaRef*wRef, wRef^2];
+
+% Alternative, falls es zu langsam ist:
+% wRef = 0.55;
+% numRef = wRef^2;
+% denRef = [1, 2*zetaRef*wRef, wRef^2];
+
+%% Kontrolle
+
+disp('--- ReglerInit Kapitel 4.4 mit Vorfilter ---')
+
+disp('K = ')
+disp(K)
+
+disp('V = ')
+disp(V)
+
+disp('K(3) = ')
+disp(K(3))
+
+disp('L_t = ')
+disp(L_t)
+
+disp('numRef = ')
+disp(numRef)
+
+disp('denRef = ')
+disp(denRef)
+
+disp('Eigenwerte A-BK:')
+disp(eig(A - B*K))
+
+disp('Eigenwerte A-LC:')
+disp(eig(A - L_t*C_t))
+
+disp('Rang Steuerbarkeit:')
+disp(rank(ctrb(A,B)))
+
+disp('Rang Beobachtbarkeit:')
+disp(rank(obsv(A,C_t)))
